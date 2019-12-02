c8d5fa73c394b7dadda4b0189b9313cf67469623
SwanHub <jdprince555@gmail.com>
2019-12-02 15:04:08 -0800

    pipelining and dates

---
+++ 01-templates/015-funcInTemplate/main.go
@@ -0,0 +1,12 @@
+package main
+
+// not convinced of the use of this video since all of my work in Go is backend
+// deciding not to work through the example
+// if for some reason this comes up in the future, Todd's video is at this url:
+// https://www.udemy.com/course/go-programming-language/learn/lecture/5989724#overview
+
+import "fmt"
+
+func main() {
+	fmt.Println("we here")
+}
