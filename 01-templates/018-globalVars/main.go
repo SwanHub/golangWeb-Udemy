ae708c89d4ca1a2d9c7130029d8811a6e299b47e
SwanHub <jdprince555@gmail.com>
2019-12-02 15:16:28 -0800

    nested templates

---
+++ 01-templates/018-globalVars/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// another instance of something I don't think is particularly useful for my purposes
+// these are predefined global functions that come with the `text/template` package
+// for a full list go here: https://godoc.org/text/template#hdr-Functions
+// and for Todd's video, here:
+// https://www.udemy.com/course/go-programming-language/learn/lecture/5995886#overview
+func main() {
+	fmt.Println("we here")
+}
---
+++ 01-templates/019-nestedTemplates/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// a third instance of info which is irrelevant to my purposes
+// this one is about nested templates, essentially html snippets inside html snippets
+// video, in case ever curious:
+// https://www.udemy.com/course/go-programming-language/learn/lecture/5995952#overview
+// Only possible use case I could give any of this information is sending it to the screen.
+// But even that is highly unlikely, given that I'll always be sending JSON to the screen
+// Still, good to know it's here and possible I suppose
+
+func main() {
+	fmt.Println("we here")
+}
